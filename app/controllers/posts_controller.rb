117f9fe944165f9ec16b33e2b243fc336670a1e4
LR <lr177245@gmail.com>
2020-08-24 13:29:54 -0700

    end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,62 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :vote]
+  before_action :require_user, except: [:index, :show]
+
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by { |x| x.total_votes }.reverse
   end
 
   def show
-    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = current_user
+
+    if @post.save
+      flash[:notice] = 'Your post was created'
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      flash[:notice] = 'The post was updated'
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def vote
+    vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+
+    if vote.valid?
+      flash[:notice] = 'Your vote was counted'
+    else
+      flash[:error] = 'You can only vote once on a post'
+    end
+
+    redirect_to :back
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description, category_ids: [])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def fix_url(str)
+    if !str || str.empty?
+      return ''
+    else
+      str.starts_with?('http://') ? 'str' : "http://#{str}"
+    end
+  end
+
+  def display_datetime(dt) 
+    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
